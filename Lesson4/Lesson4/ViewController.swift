ade7d3e56bbc6bc6ba140130626646b7d557088c
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-02-29 20:22:51 +0100

    Add elements and actions
    Add Label, Button, and segmentControl to the View. Add and  describe actions for Button and segmentControl

--- Lesson4/Lesson4/ViewController.swift
+++ Lesson4/Lesson4/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLable: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        welcomeLable.text = "Welcome!"
     }
 
 
+    @IBAction func tapMeButtonAction(_ sender: UIButton) {
+        welcomeLable.text = "Did Tap"
+    }
+    
+    
+    @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
+        welcomeLable.text = "\(sender.titleForSegment(at: sender.selectedSegmentIndex) ?? "")"
+        
+    }
 }
 
